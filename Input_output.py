1357d7ffa18da5d06462c029bd5d955842355ecd
HeyArtem <artem_white@mail.ru>
2020-05-15 03:41:30 +0300

    ДЗ №1 Рожков Артем  и еще

---
+++ Input_output.py
@@ -0,0 +1,18 @@
+# однострочный комент
+'''
+перв строка
+втор строка
+
+'''
+print('hello! ')
+print('hello!', 'Student!' )
+print('hello!', 'Student!', 123, sep = 'xxx')
+
+print('hello!', 'Student!', 123, end = 'yyy')
+
+print()
+
+# Ввод
+age = input ('Пожалуйста введи age ')
+print(age, type(age))
+print(int(age), type(int(age)))
